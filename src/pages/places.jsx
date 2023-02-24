b313aa27b715702e8b1330f87f94d9e3f89e9c28
Jethro Williams <jethrowilliams@outlook.com>
2023-02-24 18:38:36 +0700

    Add dummy table
    Works as wanted on larger screens, but the left columns still scrolls on smaller screens

--- src/pages/places.jsx
+++ src/pages/places.jsx
@@ -2,6 +2,26 @@ import Head from 'next/head'
 
 import SimpleLayout from '@/components/SimpleLayout'
 
+const Thailand = 'Thailand'
+
+const PlacesData = [
+  {
+    place: 'Bangkok',
+    country: Thailand,
+    lastVisited: '2022',
+    criteria: {
+      affordable: {
+        verdict: true,
+        description: 'Bangkok is very affordable because...',
+      },
+      internet: {
+        verdict: true,
+        description: 'The Internet in Bangkok is...',
+      },
+    },
+  },
+]
+
 const Places = () => {
   return (
     <>
@@ -9,13 +29,55 @@ const Places = () => {
         <title>Places</title>
         <meta
           name="description"
-          content="A condensed summary of digital nomad destinations"
+          content="A summary of digital nomad destinations."
         />
       </Head>
       <SimpleLayout
-        title="Places"
-        intro="An summary of digital nomad destinations."
-      ></SimpleLayout>
+        title="Where I've been and what I thought of it"
+        intro="A summary of digital nomad destinations."
+      >
+        <div className="no-scrollbar relative overflow-scroll rounded-2xl border border-zinc-100 p-6 pl-0 dark:border-zinc-700/40">
+          <div className="grid w-full grid-cols-[100px_repeat(6,200px)] items-center justify-items-center">
+            <div className="sticky left-0 h-full w-full bg-white pl-6 dark:bg-zinc-900"></div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+              Affordable
+            </div>
+            <div className="sticky left-0 w-full justify-self-start bg-white pl-6 dark:bg-zinc-900">
+              <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                Bangkok
+              </div>
+              <div className="text-xs text-zinc-500 dark:text-zinc-400">
+                Thailand
+              </div>
+            </div>
+            <div>✅</div>
+            <div>✅</div>
+            <div>✅</div>
+            <div>✅</div>
+            <div>✅</div>
+            <div>✅</div>
+          </div>
+        </div>
+
+        {/* TODO: Add a table - This displays the 'verdicts' as ✅ or ❌. Hover over an icon to display the description */}
+        {/* It'd be cool for smaller screens, if the left column stays in place while the rest of the table scrolls horizontally */}
+        {/* TODO: Add a section about each place, with the verdict and description for each criteria. Possibly also add a summary here. */}
+      </SimpleLayout>
     </>
   )
 }
