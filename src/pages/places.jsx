635d7a412f98adf50511b93c1c83df23e050d481
Jethro Williams <jethrowilliams@outlook.com>
2023-02-25 16:32:09 +0700

    Update data format and place into table

--- src/pages/places.jsx
+++ src/pages/places.jsx
@@ -1,26 +1,74 @@
+import { Fragment } from 'react'
 import Head from 'next/head'
 
 import SimpleLayout from '@/components/SimpleLayout'
 
-const Thailand = 'Thailand'
+const thailand = 'Thailand'
 
-const PlacesData = [
+const place = 'place'
+const country = 'country'
+const lastVisited = 'lastVisited'
+const criteria = 'criteria'
+const articlePath = 'articlePath'
+
+const affordable = 'affordable'
+const internet = 'internet'
+const running = 'running'
+const veganFood = 'veganFood'
+const safe = 'safe'
+const social = 'social'
+const walking = 'walking'
+
+const verdict = 'verdict'
+const description = 'description'
+
+const criteriaHeadings = {
+  [affordable]: 'Affordable',
+  [internet]: 'Fast and reliable Internet',
+  [running]: 'Good places to run',
+  [veganFood]: 'Good availability and variety of vegan food',
+  [safe]: 'Safe',
+  [social]: 'Sufficient social opportunities',
+  [walking]: 'Pleasant to walk around',
+}
+
+const placesData = [
   {
-    place: 'Bangkok',
-    country: Thailand,
-    lastVisited: '2022',
-    criteria: {
-      affordable: {
-        verdict: true,
-        description: 'Bangkok is very affordable because...',
+    [place]: 'Bangkok',
+    [country]: thailand,
+    [lastVisited]: '2022',
+    [criteria]: {
+      [affordable]: {
+        [verdict]: true,
+        [description]: 'Bangkok is very affordable because...',
       },
-      internet: {
-        verdict: true,
-        description: 'The Internet in Bangkok is...',
+      [internet]: {
+        [verdict]: true,
+        [description]: 'The Internet in Bangkok is...',
+      },
+      [running]: {
+        [verdict]: true,
+      },
+      [veganFood]: {
+        [verdict]: true,
+      },
+      [safe]: {
+        [verdict]: true,
+      },
+      [social]: {
+        [verdict]: true,
+      },
+      [walking]: {
+        [verdict]: true,
       },
     },
+    [articlePath]: '',
   },
 ]
+
+const gridColsClass = `grid-cols-[minmax(min-content,1fr)_repeat(${
+  Object.keys(criteriaHeadings).length
+},minmax(150px,2fr))]` // grid-cols-[minmax(min-content,1fr)_repeat(7,minmax(150px,2fr))]
 
 const Places = () => {
   return (
@@ -34,43 +82,40 @@ const Places = () => {
       </Head>
       <SimpleLayout
         title="Where I've been as a digital nomad and what I thought of it"
-        intro="A summary of digital nomad destinations."
+        intro="A massive oversimplification of each place as a digital nomad destination."
       >
         <div className="no-scrollbar relative overflow-scroll rounded-2xl border border-zinc-100 p-6 pl-0 dark:border-zinc-700/40">
-          <div className="grid w-min grid-cols-[minmax(min-content,1fr)_repeat(6,minmax(150px,2fr))] items-center justify-items-center">
+          <div
+            className={`grid w-min ${gridColsClass} items-center justify-items-center gap-y-6 gap-x-2`}
+          >
             <div className="sticky left-0 h-full w-full bg-gradient-to-r from-white via-white to-transparent px-6 dark:from-zinc-900 dark:via-zinc-900"></div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-              Affordable
-            </div>
-            <div className="sticky left-0 w-full justify-self-start bg-gradient-to-r from-white via-white to-transparent px-6 dark:from-zinc-900 dark:via-zinc-900">
-              <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                Bangkok
+            {Object.values(criteriaHeadings).map((heading) => (
+              <div
+                key={`${heading}-table-heading`}
+                className="self-end text-center text-sm font-medium text-zinc-900 dark:text-zinc-100"
+              >
+                {heading}
               </div>
-              <div className="text-xs text-zinc-500 dark:text-zinc-400">
-                Thailand
-              </div>
-            </div>
-            <div>✅</div>
-            <div>✅</div>
-            <div>✅</div>
-            <div>✅</div>
-            <div>✅</div>
-            <div>✅</div>
+            ))}
+            {placesData.map((placeData) => (
+              <Fragment key={`${placeData[place]}-${placeData[country]}-data`}>
+                <div className="sticky left-0 w-full justify-self-start bg-gradient-to-r from-white via-white to-transparent px-6 dark:from-zinc-900 dark:via-zinc-900">
+                  <div className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                    {placeData[place]}
+                  </div>
+                  <div className="text-xs text-zinc-500 dark:text-zinc-400">
+                    {placeData[country]}
+                  </div>
+                </div>
+                {Object.keys(placeData[criteria]).map((criterion) => (
+                  <div
+                    key={`${placeData[place]}-${placeData[country]}-${criterion}-data`}
+                  >
+                    {placeData[criteria][criterion][verdict] ? '✅' : '❌'}
+                  </div>
+                ))}
+              </Fragment>
+            ))}
           </div>
         </div>
 
