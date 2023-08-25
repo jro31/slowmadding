709407f293022e4d0256fda0bac4766039f0fd02
Jethro Williams <jethrowilliams@outlook.com>
2023-08-25 17:54:46 +0300

    Add backpacking trip navigation

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -19,16 +19,28 @@ import TimelineComponent from '@/components/TimelineComonent'
 
 import { parsedTimelineData } from '@/lib/timelineData'
 
-const digitalNomadStartDate = '2022-10-07'
+import clsx from 'clsx'
 
-const backpackingStartDate = '2008-11-20'
-const backpackingEndDate = '2018-11-29'
+const digitalNomad = 'Digital nomad'
+const backpackingTrip = 'Backpacking trip'
+const startDate = 'Start date'
+const endDate = 'End date'
 
 const Timeline = () => {
-  const [timelineStartDate, setTimelineStartDate] = useState(
-    digitalNomadStartDate
-  )
-  const [timelineEndDate, setTimelineEndDate] = useState(undefined)
+  const [currentTimeline, setCurrentTimeline] = useState(digitalNomad)
+
+  const timelineTypes = [digitalNomad, backpackingTrip]
+
+  const timelineDates = {
+    [digitalNomad]: {
+      [startDate]: '2022-10-07',
+      [endDate]: undefined,
+    },
+    [backpackingTrip]: {
+      [startDate]: '2008-11-20',
+      [endDate]: '2018-11-29',
+    },
+  }
 
   return (
     <>
@@ -40,8 +52,33 @@ const Timeline = () => {
         title="Timeline"
         intro="Where being a digital nomad has taken me."
       >
+        <div className="mb-3 flex flex-1 justify-center">
+          <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+            {timelineTypes.map((type) => (
+              <li key={`${type}-timeline`}>
+                <div
+                  onClick={(e) => setCurrentTimeline(e.target.textContent)}
+                  className={clsx(
+                    'relative block cursor-pointer px-3 py-2 transition',
+                    currentTimeline === type
+                      ? 'text-teal-500 dark:text-teal-400'
+                      : 'hover:text-teal-500 dark:hover:text-teal-400'
+                  )}
+                >
+                  {type}
+                  {currentTimeline === type && (
+                    <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
         <TimelineComponent
-          timelineData={parsedTimelineData(timelineStartDate, timelineEndDate)}
+          timelineData={parsedTimelineData(
+            timelineDates[currentTimeline][startDate],
+            timelineDates[currentTimeline][endDate]
+          )}
         />
       </SimpleLayout>
     </>
