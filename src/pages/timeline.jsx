a6f8efea3bac194f585e80ac13a377a80471ad03
Jethro Williams <jethrowilliams@outlook.com>
2023-09-20 18:15:48 +0300

    Swap-over icons

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -142,12 +142,12 @@ const Timeline = ({ timelines }) => {
           </div>
           <div className="absolute right-0 flex flex-initial items-center gap-4 rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20">
             {ascending ? (
-              <ArrowUpIcon
+              <ArrowDownIcon
                 onClick={handleAscendingToggle}
                 className={toggleButtonClassnames}
               />
             ) : (
-              <ArrowDownIcon
+              <ArrowUpIcon
                 onClick={handleAscendingToggle}
                 className={toggleButtonClassnames}
               />
