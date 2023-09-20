c4527f658467580383c821e80f8f470a9d5bb23b
Jethro Williams <jethrowilliams@outlook.com>
2023-09-20 17:54:21 +0300

    Reverse timeline order on button click

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -11,9 +11,6 @@
 // - Hide these in 'Compact' mode?
 // TODO: Add the accommodation type somehow - feasibly using a logo in the bullet, such as a camping logo or an Airbnb logo
 // TODO: There should be more empty space between stays (and countries) if there's a blank day or two between them
-// TODO: You should be able to flip the direction of the timeline, so you can view it descending or ascending
-// TODO: There should be a 'Compact' mode (as opposed to 'Detailed' mode), where all stays are the same length
-// - This should perhaps be the default on mobile
 // TODO: You should be able to filter the timeline by a particular country/stay, for example if I just want to see my stays in Bangkok or in China, I can select these from a dropdown
 // - This should be pan-timeline, so if I select 'Bangkok', it shows me my stays in Bangkok from ALL timelines, perhaps under new 'timeline' headings, such as 'Digital nomad' or 'Backpacking trip'
 
@@ -171,6 +168,7 @@ const Timeline = ({ timelines }) => {
 
         <TimelineComponent
           timelineData={timelines[currentTimeline].timelineData}
+          ascending={ascending}
           compactMode={compactMode}
         />
       </SimpleLayout>
