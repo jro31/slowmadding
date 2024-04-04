ffa64c7f7eeab41154c698056a2219c10cffedcf
Jethro Williams <jethrowilliams@outlook.com>
2024-04-04 15:49:29 +0800

    Fix display bugs

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,3 +1,9 @@
+// TODO: Add swipe to change images on mobile
+// TODO: Add transitions to both image swipes and 'step' dots
+// TODO: Add links to step dots to click to specific images
+// TODO: Handle if there are so many images that the step dots are wider than the article
+// TODO: Remove chevrons from images (or replace them with something more sightly)
+
 import { useEffect, useState } from 'react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
@@ -47,7 +53,11 @@ const ImageGallery = ({ images }) => {
 
   return (
     <>
-      <div className={`relative h-[${galleryHeight}px]`}>
+      <div
+        className={`relative h-[${galleryHeight}px]${
+          images.length > 1 ? ' mb-0' : ''
+        }`}
+      >
         <div
           className={`absolute flex h-full w-full ${
             firstImageIsShown() ? 'justify-end' : 'justify-between'
@@ -84,39 +94,47 @@ const ImageGallery = ({ images }) => {
         </div>
       </div>
 
-      <nav
-        className="not-prose flex items-center justify-center"
-        aria-label="Progress"
-      >
-        <ol role="list" className="flex list-none items-center space-x-5">
-          {images.map((image, index) => (
-            <li key={image.src.src}>
-              {imageIndex === index ? (
-                <div
-                  className="relative flex items-center justify-center"
-                  aria-current="step"
-                >
-                  <span
-                    className="absolute flex h-4 w-4 p-px"
-                    aria-hidden="true"
+      {images.length > 1 && (
+        <nav
+          className="not-prose mt-3 flex items-center justify-center"
+          aria-label="Progress"
+        >
+          <ol
+            role="list"
+            className="flex w-full list-none items-center justify-center"
+          >
+            {images.map((image, index) => (
+              <li
+                className="flex shrink grow-0 basis-5 justify-center"
+                key={image.src.src}
+              >
+                {imageIndex === index ? (
+                  <div
+                    className="relative flex items-center justify-center"
+                    aria-current="step"
                   >
-                    <span className="h-full w-full rounded-full bg-zinc-600 dark:bg-zinc-400" />
-                  </span>
-                  <span
-                    className="relative block h-2.5 w-2.5 rounded-full bg-zinc-400 dark:bg-zinc-500"
-                    aria-hidden="true"
-                  />
-                  {/* <span className="sr-only">{step.name}</span> */}
-                </div>
-              ) : (
-                <div className="block h-2.5 w-2.5 rounded-full bg-zinc-400 hover:bg-zinc-600 dark:bg-zinc-500 hover:dark:bg-zinc-400">
-                  {/* <span className="sr-only">{step.name}</span> */}
-                </div>
-              )}
-            </li>
-          ))}
-        </ol>
-      </nav>
+                    <span
+                      className="absolute flex h-4 w-4 p-px"
+                      aria-hidden="true"
+                    >
+                      <span className="h-full w-full rounded-full bg-zinc-600 dark:bg-zinc-400" />
+                    </span>
+                    <span
+                      className="relative block h-2.5 w-2.5 rounded-full bg-zinc-400 dark:bg-zinc-500"
+                      aria-hidden="true"
+                    />
+                    {/* <span className="sr-only">{step.name}</span> */}
+                  </div>
+                ) : (
+                  <div className="block h-2.5 w-2.5 rounded-full bg-zinc-400 hover:bg-zinc-600 dark:bg-zinc-500 hover:dark:bg-zinc-400">
+                    {/* <span className="sr-only">{step.name}</span> */}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ol>
+        </nav>
+      )}
 
       <LinkWrapper url={images[imageIndex].url}>
         <p className="text-center">{images[imageIndex].caption}</p>
