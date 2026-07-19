10edc3741fa11709a4d974d55f214245b71f1079
Jethro Williams <jethrowilliams@outlook.com>
2026-07-19 18:47:01 +0300

    Use default Tailwind classes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,12 +7,12 @@ const NotFound = () => {
         <title>404: This page could not be found</title>
       </Head>
       <div className="flex h-screen flex-col items-center justify-center text-center text-zinc-800 dark:text-zinc-100">
-        <div className="leading-[48px]">
-          <h1 className="mr-5 inline-block border-r border-black/30 pr-[23px] align-top text-2xl leading-[48px] font-medium dark:border-white/30">
+        <div className="leading-12">
+          <h1 className="mr-5 inline-block border-r border-black/30 pr-5.75 align-top text-2xl leading-12 font-medium dark:border-white/30">
             404
           </h1>
           <div className="inline-block">
-            <h2 className="text-sm leading-[28px] font-normal">
+            <h2 className="text-sm leading-7 font-normal">
               This page could not be found.
             </h2>
           </div>
